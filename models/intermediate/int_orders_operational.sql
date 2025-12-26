f42f5ffe62a236cc7165021d7ca2c4ff3ac009c5
Adil Poyraz AKA <workintech.poyrazaka@gmail.com>
2025-12-26 13:25:38 +0000

    Add finance_days mart model

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -10,6 +10,13 @@ orders_margin_data AS (
 
 SELECT orders_margin_data.orders_id,
 orders_margin_data.date_date,
+orders_margin_data.revenue,
+orders_margin_data.quantity,
+orders_margin_data.purchase_cost,
+orders_margin_data.margin,
+ship_data.shipping_fee,
+ship_data.log_cost,
+ship_data.ship_cost,
 margin + shipping_fee - log_cost - ship_cost AS operational_margin
 FROM orders_margin_data
 LEFT JOIN ship_data ON orders_margin_data.orders_id = ship_data.orders_id
